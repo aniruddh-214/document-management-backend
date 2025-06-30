51e5b21a0196a3eb2c44d6e0a984da3a6586dbb2
Aniruddh <aniruddh161680@gmail.com>
2025-06-30 22:03:05 +0530

    Fix user schema and user type files

--- src/user/schemas/request/user.schema.ts
+++ src/user/schemas/request/user.schema.ts
@@ -3,9 +3,8 @@ import { z } from 'zod';
 import { DatabaseSortingOrder } from '../../../common/enums/dbOrderSort.enum';
 import UserRoleEnum from '../../../common/enums/role.enum';
 import ZodHelper from '../../../common/helpers/zod.helper';
-import { USER_CONSTANTS } from '../../constants/user.constant';
+import { USER_CONSTANTS, userEntityKeys } from '../../constants/user.constant';
 import UserEntity from '../../entities/user.entity';
-import { userEntityKeys } from '../../types/request/getUser.type';
 
 const { VALIDATION_MESSAGES } = USER_CONSTANTS.REQUEST;
 
@@ -155,6 +154,12 @@ export const UsersSchema = z.object({
       id: z.string().uuid({ message: VALIDATION_MESSAGES.INVALID_USER_ID }),
     }),
   }),
+
+  getUserDocuments: z.object({
+    param: z.object({
+      id: z.string().uuid({ message: VALIDATION_MESSAGES.INVALID_USER_ID }),
+    }),
+  }),
 });
 
 export type GetAllUSersRequestQueryType = z.infer<
@@ -174,4 +179,8 @@ export type UpdateUserDetailsRequestBodyType = z.infer<
 
 export type DeleteUserRequestParamType = z.infer<
   typeof UsersSchema.shape.deleteUser.shape.param
+>;
+
+export type getUserDocumentsRequestParamType = z.infer<
+  typeof UsersSchema.shape.getUserDocuments.shape.param
 >;
